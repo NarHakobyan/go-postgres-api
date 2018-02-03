8adc8f8f581501d2baeda0875f3a1100185b686d
Narek <narek.hakobyan.07@gmail.com>
2018-02-04 01:05:36 +0400

    change glide vendor tool with dep, and add config functionality

---
+++ config/index.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/spf13/viper"
+)
+
+type config struct {
+	Server struct {
+		Port string
+	}
+}
+
+var C config
+
+func init() {
+	configFile, err := filepath.Abs("./config")
+
+	if err != nil {
+		panic(fmt.Errorf("Unable find config directory: %s \n", err))
+	}
+
+	viper.SetDefault("env", "development")
+
+	viper.SetEnvPrefix("app")
+	viper.AutomaticEnv()
+
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(configFile)
+	env := viper.Get("env").(string)
+	viper.SetConfigName(env)
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	err = viper.Unmarshal(&C)
+	if err != nil {
+		fmt.Println("Unable to decode into struct, %v", err)
+	}
+}
--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-	"github.com/narhakobyan/go-pg-api/router"
-	"github.com/gin-gonic/gin"
 	"net/http"
-	"github.com/narhakobyan/go-pg-api/database"
+
+	"github.com/gin-gonic/gin"
+	"github.com/narhakobyan/go-pg-api/config"
+	"github.com/narhakobyan/go-pg-api/router"
 )
 
 func main() {
 
-	defer database.Db.Close();
-
 	router.Router.GET("/", func(context *gin.Context) {
 		context.String(http.StatusOK, "WORKING")
 	})
-	router.Router.Run(":8000")
+	router.Router.Run(":" + config.C.Server.Port)
 }
